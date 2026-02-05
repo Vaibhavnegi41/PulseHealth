1757bb1bffb3b06eef110425676a7c7b79b0043e
Vaibhavnegi41 <negivaibhav2005@gmail.com>
2026-02-05 18:15:04 +0530

    c

--- backend/test_model.py
+++ backend/test_model.py
@@ -5,18 +5,14 @@ diabetes_model = joblib.load("diabetes_model.pkl")
 heart_model = joblib.load("heart_model.pkl")
 scaler = joblib.load("scaler.pkl")
 
-# Format:
-# HighBP, HighChol, CholCheck, BMI, Smoker, Stroke,
-# PhysicalActivity, Alcohol, AnyHealthcare, NoDocCost,
-# GenHlth, MentHlth, Sex, Age, Income
 
-# Healthy athlete
+
 person1 = [[0,0,1,22,0  ,0,1,0,1,0,2,0,1,4,8]]
 
-# Unhealthy inactive smoker
+
 person2 = [[1,1,0,35,1,0,0,1,0,1,4,15,1,9,3]]
 
-# Obese, no gym, high BP
+
 person3 = [[1,1,1,40,0,0,0,0,1,0,4,10,0,8,4]]
 
 p1 = scaler.transform(person1)
--- backend/train_model.py
+++ backend/train_model.py
@@ -5,17 +5,13 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 
-print("\n🚀 Training AI Health Prediction Models...\n")
+print("\nTraining AI Health Prediction Models...\n")
 
-# ===============================
-# 1. Load Dataset
-# ===============================
+
 data = pd.read_csv("health_dataset.csv")
 print("Dataset Loaded:", data.shape)
 
-# ===============================
-# 2. Select Input Features
-# ===============================
+
 features = [
     "HighBP", "HighChol", "CholCheck", "BMI", "Smoker",
     "Stroke", "PhysicalActivity", "HvyAlcoholConsump",
@@ -25,22 +21,18 @@ features = [
 
 X = data[features]
 
-# Targets
+
 y_diabetes = data["Diabetes_binary"]
 y_heart = data["HeartDiseaseorAttack"]
 
-# ===============================
-# 3. Scale the Features
-# ===============================
+
 scaler = StandardScaler()
 X_scaled = scaler.fit_transform(X)
 
 joblib.dump(scaler, "scaler.pkl")
 print("Scaler saved")
 
-# ===============================
-# 4. Train Diabetes Model
-# ===============================
+
 X_train, X_test, y_train, y_test = train_test_split(X_scaled, y_diabetes, test_size=0.2, random_state=42)
 
 diabetes_model = RandomForestClassifier(n_estimators=200, max_depth=12)
@@ -52,9 +44,6 @@ print("Diabetes Model Accuracy:", accuracy_score(y_test, pred))
 joblib.dump(diabetes_model, "diabetes_model.pkl")
 print("Diabetes Model Saved")
 
-# ===============================
-# 5. Train Heart Disease Model
-# ===============================
 X_train, X_test, y_train, y_test = train_test_split(X_scaled, y_heart, test_size=0.2, random_state=42)
 
 heart_model = RandomForestClassifier(n_estimators=200, max_depth=12)
@@ -66,10 +55,7 @@ print("Heart Disease Model Accuracy:", accuracy_score(y_test, pred))
 joblib.dump(heart_model, "heart_model.pkl")
 print("Heart Disease Model Saved")
 
-# ===============================
-# 6. Done
-# ===============================
-print("\n🎉 AI Health System Ready!")
+print("\n AI Health System Ready!")
 print("Generated Files:")
 print("- diabetes_model.pkl")
 print("- heart_model.pkl")
